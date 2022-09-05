4c000a251b2aa4caa2b9e2a84db487eeb59730c4
SayakaIw <160105syk@gmail.com>
2022-09-05 12:26:04 +0000

    relation_search_end

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  #フォローする側から見て
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  #フォローされる側から見て
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
 
   has_one_attached :profile_image
@@ -19,19 +25,31 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
-  # 検索方法分岐
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @user = User.where("name LIKE?", "#{word}")
-    elsif search == "forward_match"
-      @user = User.where("name LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @user = User.where("name LIKE?","%#{word}")
-    elsif search == "partial_match"
-      @user = User.where("name LIKE?","%#{word}%")
+
+   # 検索方法分岐
+  def self.search_for(content,method)
+    if method == 'perfect'
+      Book.where(title:content)
+    elsif method == 'forword'
+      Book.where('title LIKE?', content + '%')
+    elsif method == 'backward'
+      Book.where('title LIKE?', '%' + content)
     else
-      @user = User.all
+      Book.where('title LIKE?', '%' + content + '%')
     end
   end
+
+  #フォローした時の処理
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+  #フォロー外す時の処理
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+  #フォローしてるか判定
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
